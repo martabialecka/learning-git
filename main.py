cf0b0671147bafd96279d02769636dbe1edecc38
martabialecka <121777424+martabialecka@users.noreply.github.com>
2023-01-03 02:04:18 +0100

    Fix sketch info 2

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-lista_zakupów = {
-"piekarnia" : ["chleb", "pączek", "bułki"],
-"warzywniak" : ["marchew", "seler", "rukola"]
-}
-#test
-print ("Lista zakupów")
-s = 0
-for i in lista_zakupów.items():
-    print (f"Idę do {i[0].capitalize()}. Kupuję tu następujące rzeczy: {[towar.capitalize() for towar in i[1]]}")
-    s = s + len (i[1])
-print (f"W sumie kupuję {s} produktów.")
-print ("'Hiszpańska inkwizycja' to najlepszy skecz grupy Monty Pythona")
+print ("'Hiszpańska inkwizycja' to najlepszy sketch grupy Monty Pythona")
